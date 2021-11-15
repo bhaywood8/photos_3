388dcd1732748e1cd855e02b1a4c43c150b8c34c
anonymous anonymous <haywoodb@gmail.com>
2021-11-15 19:42:09 +0000

    Indirect association between users and photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -11,6 +11,10 @@ class Photo < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :user_comments,
+             :through => :comments,
+             :source => :user
+
   has_many   :user_likes,
              :through => :likes,
              :source => :user
