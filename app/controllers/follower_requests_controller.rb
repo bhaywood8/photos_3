70b29bd31e378efb9432296760dd034e3dc76351
anonymous anonymous <haywoodb@gmail.com>
2021-11-15 19:42:33 +0000

    Display associated collections on show pages

--- app/controllers/follower_requests_controller.rb
+++ app/controllers/follower_requests_controller.rb
@@ -42,8 +42,14 @@ class FollowerRequestsController < ApplicationController
   # DELETE /follower_requests/1
   def destroy
     @follower_request.destroy
-    redirect_to follower_requests_url, notice: 'Follower request was successfully destroyed.'
+    message = "FollowerRequest was successfully deleted."
+    if Rails.application.routes.recognize_path(request.referrer)[:controller] != Rails.application.routes.recognize_path(request.path)[:controller]
+      redirect_back fallback_location: request.referrer, notice: message
+    else
+      redirect_to follower_requests_url, notice: message
+    end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
